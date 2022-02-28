d25f42f916f2583b07f669bbc58e5c5b6297363b
mojvaf <mvafa44@gmail.com>
2022-02-28 09:09:14 +0100

    login page

--- src/webapp/src/components/auth/login/Login.tsx
+++ src/webapp/src/components/auth/login/Login.tsx
@@ -2,5 +2,58 @@ import React from "react";
 import "../../../styles/login/Login.css";
 
 export const Login = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div>
+          <h3 className="fw-bold text-center fst-normal">Zula</h3>
+          <p className="text-center fst-normal">the Reading Tribe</p>
+        </div>
+        <div className="mb-5">
+          <h1 className="fw-bold text-center fst-normal">
+            Welcome!
+            <br /> Its Good To See You.
+          </h1>
+        </div>
+        <form className="d-flex flex-column align-items-center w-50">
+          <div className=" w-100">
+            <div>
+              <small>Name</small>
+              <input
+                className="form-control my-3 p-3"
+                type="text"
+                placeholder="Name"
+                name="name"
+                required
+              />
+            </div>
+          </div>
+          <div className="w-100">
+            <div>
+              <small>Password</small>
+              <input
+                className="form-control my-3 p-3"
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+              />
+            </div>
+          </div>
+          <div className="w-100">
+            <div>
+              <small className="small-dark">
+                Did you forget your password?
+              </small>
+            </div>
+          </div>
+          <div className="login-buttons mt-5 ">
+            <a href="/" className="button fw-bold ">
+              login
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
