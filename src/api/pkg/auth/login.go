1c11ec798918dc4ff5199e642047b25e2bb15e90
Robert Focke <robert_focke@yahoo.com>
2022-03-08 13:30:50 +0100

    deprecate unused code

--- src/api/pkg/auth/login.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "github.com/reading-tribe/zula/pkg/models"
-
-func Login(user models.User) models.Session {
-	return models.Session{}
-}
--- src/api/pkg/auth/register.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "github.com/reading-tribe/zula/pkg/models"
-
-func Register(email models.Email) models.User {
-	return models.User{}
-}
